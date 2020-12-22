2663fd6f2ed69594d38978b067a4ac759a747b64
Patrik Votoček <patrik@votocek.cz>
2020-12-22 17:56:31 +0100

    update versions

--- versions.tf
+++ versions.tf
@@ -1,3 +1,14 @@
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13.4, < 0.15.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.22.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.13.3"
+    }
+  }
 }
